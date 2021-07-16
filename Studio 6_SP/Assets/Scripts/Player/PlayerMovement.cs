853a3bb05afef97036cbf6387473ef04a9fdabef
Philbax1 <phillip.john.baxter@gmail.com>
2021-07-16 16:30:23 +1200

    Having git issues, trying to commit work for, fixed camera not working working with mouse, added jump and sprint, sunday will alter jump to allow double jump then sprint finished for Monday

--- Studio 6_SP/Assets/Scripts/Player/PlayerMovement.cs
+++ Studio 6_SP/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,10 +7,20 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    float speed = 6f;
+    float runSpeed = 20f;
+    float jumpHeight = 2f;
 
-    public float turnSmoothTime = 0.1f;
+    float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    float gravity = -20f;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
 
     // Start is called before the first frame update
@@ -22,11 +32,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
+        float horizontal = Input.GetAxisRaw("Horizontal");  // -1 if a key, +2 if d key
+        float vertical = Input.GetAxisRaw("Vertical");  // -1 if s key, +2 if w key
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        /*      Jump      */
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //checks if groundcheck sphere is in contact with object labelled ground
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //direction = new Vector3(horizontal, jumpHeight, vertical);
+            //controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        
+
+        /*      0000        */
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -36,5 +66,11 @@ public class PlayerMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        
+        /*      Sprint        */
+        if(Input.GetKeyDown(KeyCode.LeftShift)) speed = runSpeed;
+        if(Input.GetKeyUp(KeyCode.LeftShift)) speed = 6f;
+        
     }
 }
