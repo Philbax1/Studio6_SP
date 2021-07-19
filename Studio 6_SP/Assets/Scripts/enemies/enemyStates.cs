87ce9aeb3d9550117ae491f69679e2964ef4e8d1
Phillip Baxter (1000024452) <BAXTPJ1@student.op.ac.nz>
2021-07-19 17:01:55 +1200

    using polytech machine updating .net sdk

--- Studio 6_SP/Assets/Scripts/enemies/enemyStates.cs
+++ Studio 6_SP/Assets/Scripts/enemies/enemyStates.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class enemyStates : MonoBehaviour
 {
+    public transform player;
+    public navmesh
+
+    float followRange = 100f;
+    float attackRange = 10f;
+
+    float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +21,16 @@ public class enemyStates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector3(player.position, transform.position);
+
+        if(distance =< followRange)
+        {
+
+        }
+        if()
+        {
+
+        }
     }
 
     void neutralPatrol()
